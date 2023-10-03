f767a2a3b38ba095fbc46211dd52f0b5274c00c5
Ayushhad <108228706+Ayushhad@users.noreply.github.com>
2023-10-03 16:08:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let result = document.getElementById("GFG");
- 
-    function changeColor(x) {
+function changeColor(x){
         let color = 'white';
         if(x==1) color = 'blue';
         if(x==2) color = 'yellow';
@@ -8,11 +6,10 @@ let result = document.getElementById("GFG");
         if(x==4) color = 'green';
         //if(x==5) color = 'white';
         document.body.style.background = color;
-    }
+ }
     let x = 0;
     function clicked() {
         x+=1;
         if(x==6) x=1;
         changeColor(x);
-        result.innerHTML = "Background Color changed";
     }
